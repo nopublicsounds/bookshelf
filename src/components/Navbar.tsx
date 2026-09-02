9cd880e891c761d102066d8a24a8019ff410d11e
이찬희 <ch20041001@gmail.com>
2026-09-02 13:24:49 +0000

    tailwindcss 적용

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <BrowserRouter>
             <nav>
-                <h1>
+                <h1 className = "font-bold text-2xl text-black no-underline m-4">
                     <Link to='/'>내 책장</Link>
                 </h1>
             </nav>
