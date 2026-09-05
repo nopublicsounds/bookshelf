80a0bf7491856d93c483ca4659ab86167c86c3b3
이찬희 <ch20041001@gmail.com>
2026-09-05 05:48:25 +0000

    메인화면 임시 카드 그리드

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import type { Post } from './types.ts'
 import Card from './components/Card.tsx'
 import Navbar from './components/Navbar.tsx'
+import Footer from './components/Footer.tsx'
 
 export default function App({ posts }: { posts: Post[] }) {
   return (
     <>
       <Navbar />
       <main>
-        {posts.map(post => (
-          <Card key={post.id} {...post} />
-        ))}
+        <section className="grid m-4 md:grid-cols-2 lg:grid-cols-4">
+          {posts.map(post => (
+            <Card key={post.id} {...post} />
+          ))}
+        </section>
       </main>
+      <Footer />
     </>
   )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,17 @@
 import type { Post } from '../App.tsx'  
-
+import { BrowserRouter, Link } from 'react-router-dom'
 
 export default function Card(post: Post) {
     return (
-        <div>
-            <img src={post.cover} alt={`${post.title} cover`} />
-            <h3>{post.title}</h3>
-            <p>{post.author}</p>
-            <p>평점: {post.rating}</p>
-        </div>
+        <BrowserRouter>
+            <Link to={`/posts/${post.id}`}>
+                <div>
+                    <img src={post.cover} alt={`${post.title} cover`} />
+                    <h3 className="font-bold text-xl text-black m-2">{post.title}</h3>
+                    <p className="text-black m-2">{post.author}</p>
+                    <p className="text-black m-2">★ {post.rating}/5</p>
+                </div>
+            </Link>
+        </BrowserRouter>
     )
 }
